a49ab27d363a513ff2a07c78e74cf4d047839787
fwcd <fwcdmail@gmail.com>
2021-02-13 22:17:33 +0100

    Clean up launch mechanism

---
+++ Woods/WoodsApp.swift
@@ -0,0 +1,18 @@
+//
+//  WoodsApp.swift
+//  Woods
+//
+//  Created by Fredrik on 6/21/20.
+//  Copyright © 2020 Fredrik. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
