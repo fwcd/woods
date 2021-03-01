1eb9811b8f8f5c14199fe833f18839353255ce24
fwcd <fwcdmail@gmail.com>
2021-03-02 00:24:44 +0100

    Add SlideOverCard

---
+++ Woods/View/Handle.swift
@@ -0,0 +1,21 @@
+//
+//  Handle.swift
+//  Woods
+//
+//  Created by Fredrik on 3/2/21.
+//
+
+import SwiftUI
+
+// Source: https://www.mozzafiller.com/posts/swiftui-slide-over-card-like-maps-stocks
+
+struct Handle: View {
+    var handleThickness = CGFloat(5.0)
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: handleThickness / 2.0)
+            .frame(width: 40, height: handleThickness)
+            .foregroundColor(Color.secondary)
+            .padding(5)
+    }
+}
