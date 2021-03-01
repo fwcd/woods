a431012a4006616e5a57012b27105ae12cb7cb3b
fwcd <fwcdmail@gmail.com>
2021-03-01 15:11:14 +0100

    Define some gc.com endpoints

--- Woods/Connector/GeocachingComConnector.swift
+++ Woods/Connector/GeocachingComConnector.swift
@@ -6,6 +6,17 @@
 //  Copyright © 2021 Fredrik. All rights reserved.
 //
 
+import Foundation
+
+// Adapted from pycaching
+
+private let baseUrl = "http://www.geocaching.com"
+private let loginPageUrl = URL(string: "\(baseUrl)/account/signin")!
+private let searchUrl = URL(string: "\(baseUrl)/play/search")!
+private let searchMoreUrl = URL(string: "\(baseUrl)/play/search/more-results")!
+private let myLogsUrl = URL(string: "\(baseUrl)/my/logs.aspx")!
+private let apiSearchUrl = URL(string: "\(baseUrl)/api/proxy/web/search")!
+
 class GeocachingComConnector: Connector {
     func logIn(using credentials: Credentials) throws {
         fatalError("TODO")
