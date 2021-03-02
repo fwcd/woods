0bed5d3a3c47319d617ac89a4219a15a8a393986
fwcd <fwcdmail@gmail.com>
2021-03-02 04:12:03 +0100

    Set up unit tests

---
+++ WoodsTests/DegreesTests.swift
@@ -0,0 +1,16 @@
+//
+//  DegreesTests.swift
+//  WoodsTests
+//
+//  Created by Fredrik on 3/2/21.
+//  Copyright © 2021 Fredrik. All rights reserved.
+//
+
+import XCTest
+@testable import Woods
+
+class DegreesTests: XCTestCase {
+    func testDegrees() {
+        
+    }
+}
