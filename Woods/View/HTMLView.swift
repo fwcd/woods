a85a6f7730cb512947e67309c862f2bb830a3dc1
fwcd <fwcdmail@gmail.com>
2021-03-03 17:13:09 +0100

    Add HTML view

---
+++ Woods/View/HTMLView.swift
@@ -0,0 +1,27 @@
+//
+//  HTMLView.swift
+//  Woods
+//
+//  Created by Fredrik on 3/3/21.
+//  Copyright © 2021 Fredrik. All rights reserved.
+//
+
+import WebKit
+import SwiftUI
+
+struct HTMLView: UIViewRepresentable {
+    let html: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let view = WKWebView()
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = false
+        view.configuration.defaultWebpagePreferences = prefs
+        view.loadHTMLString(html, baseURL: nil)
+        return view
+    }
+    
+    func updateUIView(_ view: WKWebView, context: Context) {
+        view.loadHTMLString(html, baseURL: nil)
+    }
+}
