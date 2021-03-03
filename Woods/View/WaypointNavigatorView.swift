40125218db26dea9ad6c414a4c3fe3e18bdb1c74
fwcd <fwcdmail@gmail.com>
2021-03-03 20:17:05 +0100

    Implement WaypointNavigatorView (WIP)

--- Woods/View/WaypointNavigatorView.swift
+++ Woods/View/WaypointNavigatorView.swift
@@ -13,20 +13,35 @@ struct WaypointNavigatorView: View {
     
     @EnvironmentObject private var locationManager: LocationManager
     
-//    private var distance: Length? {
-//        locationManager.location.map { target.distance(to: Coordinates(from: $0.coordinate)) }
-//    }
+    private var location: Coordinates? {
+        locationManager.location.map { Coordinates(from: $0.coordinate) }
+    }
+    private var heading: Degrees? {
+        locationManager.heading.map { Degrees(radians: atan2($0.y, $0.x)) }
+    }
+    private var accuracy: Length? {
+        (locationManager.location?.horizontalAccuracy).map { Length(meters: $0) }
+    }
+    private var distanceToTarget: Length? {
+        location.map { $0.distance(to: target) }
+    }
+    private var headingToTarget: Degrees? {
+        heading.flatMap { h in location.map { h + $0.heading(to: target) } }
+    }
     private var distance: Length? { Length(10, .kilometers) }
     
     var body: some View {
         VStack(spacing: 40) {
             Image(systemName: "location.north.fill")
                 .font(.system(size: 128))
+                .rotationEffect(.degrees(headingToTarget?.totalDegrees ?? 0))
             VStack {
-                if let distance = distance {
+                if let distance = distanceToTarget {
                     Text(distance.description)
                         .font(.title)
-                    Text("+- accuracy")
+                }
+                if let accuracy = accuracy {
+                    Text("+- \(accuracy.description)")
                         .font(.title2)
                 }
             }
