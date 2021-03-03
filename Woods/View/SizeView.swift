ee7e531c0dabc3557fbc3c92e241fc165aba5a42
fwcd <fwcdmail@gmail.com>
2021-03-03 18:16:22 +0100

    Add SizeView

---
+++ Woods/View/SizeView.swift
@@ -0,0 +1,35 @@
+//
+//  SizeView.swift
+//  Woods
+//
+//  Created by Fredrik on 3/2/21.
+//  Copyright © 2021 Fredrik. All rights reserved.
+//
+
+import SwiftUI
+
+struct SizeView: View {
+    let rating: Int
+    let maxRating: Int
+    let width: CGFloat = 16
+    let height: CGFloat = 16
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 4) {
+            ForEach(0..<maxRating) { i in
+                Rectangle()
+                    .frame(width: width, height: ((CGFloat(i + 1) * height) / CGFloat(maxRating)))
+                    .foregroundColor(i == rating ? .primary : .secondary)
+            }
+        }
+    }
+}
+
+struct SizeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            SizeView(rating: 2, maxRating: 4)
+            SizeView(rating: 5, maxRating: 10)
+        }
+    }
+}
