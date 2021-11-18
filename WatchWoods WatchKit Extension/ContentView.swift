20118415dbe9648342b938a66d4ced682ef7eca5
fwcd <fwcdmail@gmail.com>
2021-11-18 01:25:17 +0100

    Update watch preview device

--- WatchWoods WatchKit Extension/ContentView.swift
+++ WatchWoods WatchKit Extension/ContentView.swift
@@ -18,5 +18,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("Apple Watch Series 6 - 40mm")
     }
 }
