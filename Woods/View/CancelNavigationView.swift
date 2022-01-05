cff1ee0de5884b609f9d8d322a1c00f84cf4bd09
fwcd <fwcdmail@gmail.com>
2022-01-05 12:18:14 +0100

    Add CancelNavigationView

---
+++ Woods/View/CancelNavigationView.swift
@@ -0,0 +1,40 @@
+//
+//  CancelNavigationView.swift
+//  Woods
+//
+//  Created by Fredrik on 05.01.22.
+//  Copyright © 2022 Fredrik. All rights reserved.
+//
+
+import SwiftUI
+
+struct CancelNavigationView<Inner>: View where Inner: View {
+    let title: String
+    let onCancel: () -> Void
+    @ViewBuilder let inner: () -> Inner
+    
+    var body: some View {
+        NavigationView {
+            inner()
+                .navigationTitle(title)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            onCancel()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct CancelNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CancelNavigationView(title: "Test") {
+            print("Cancelled")
+        } inner: {
+            Text("Test")
+        }
+    }
+}
