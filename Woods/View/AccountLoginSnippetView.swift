751c5fe472e3e60f7e59e105ec20dc6bb73086d8
fwcd <fwcdmail@gmail.com>
2022-04-14 14:03:39 +0200

    Add AccountLoginSnippetView

---
+++ Woods/View/AccountLoginSnippetView.swift
@@ -0,0 +1,44 @@
+//
+//  AccountLoginSnippetView.swift
+//  Woods
+//
+//  Created by Fredrik on 14.04.22.
+//
+
+import SwiftUI
+
+struct AccountLoginSnippetView: View {
+    let login: AccountLogin
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "circlebadge.fill")
+                .foregroundColor(colorOf(state: login.state))
+            AccountSnippetView(account: login.account)
+        }
+    }
+    
+    private func colorOf(state: AccountLogin.State) -> Color {
+        switch state {
+        case .connecting: return .yellow
+        case .connected: return .green
+        case .failed: return .red
+        }
+    }
+}
+
+struct AccountLoginSnippetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountLoginSnippetView(login: AccountLogin(
+            account: Account(
+                id: UUID(uuidString: "b0819a69-ceef-4323-b752-ff09a70230fd")!,
+                type: .mock,
+                credentials: Credentials(
+                    username: "test",
+                    password: "test"
+                )
+            ),
+            state: .connecting
+        ))
+    }
+}
