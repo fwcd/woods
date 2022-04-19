6615937ee2ce3aa9b902dcf13fe2646857eef295
fwcd <fwcdmail@gmail.com>
2022-04-19 20:57:58 +0200

    Add basic form controls to NewWaypointView

--- Woods/View/NewWaypointView.swift
+++ Woods/View/NewWaypointView.swift
@@ -10,10 +10,41 @@ import SwiftUI
 struct NewWaypointView: View {
     let onCommit: (Waypoint) -> Void
     
-    @State private var text: String = ""
+    @State private var waypoint = Waypoint(
+        id: "",
+        name: "",
+        location: Coordinates(latitude: 0, longitude: 0)
+    )
     
     var body: some View {
-        Text("TODO")
+        Form {
+            Section("Waypoint") {
+                TextField("ID (e.g. GC-Code)", text: $waypoint.id)
+                TextField("Name", text: $waypoint.id)
+            }
+            
+            Section("Coordinates") {
+                // TODO: Factor out into view that binds a Coordinates
+                // TODO: Let user edit in other formats (e.g. decimal minutes, DMS, ...)
+                // TODO: Use numberPad keyboard
+                
+                TextField("Latitude", text: stringBinding(for: $waypoint.location.latitude))
+                TextField("Longitude", text: stringBinding(for: $waypoint.location.longitude))
+            }
+            
+            // TODO: Other metadata
+        }
+    }
+    
+    private func stringBinding(for binding: Binding<Degrees>) -> Binding<String> {
+        Binding(
+            get: { String(binding.wrappedValue.totalDegrees) },
+            set: {
+                if let degrees = Double($0).map(Degrees.init(degrees:)) {
+                    binding.wrappedValue = degrees
+                }
+            }
+        )
     }
 }
 
