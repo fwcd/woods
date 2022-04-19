6eaca83fa6f7593da7050dbc5019ee8b3cde8a2b
fwcd <fwcdmail@gmail.com>
2022-04-20 00:41:12 +0200

    Stub out CoordinateEditor

--- Woods/View/CoordinateEditor.swift
+++ Woods/View/CoordinateEditor.swift
@@ -5,19 +5,25 @@
 //  Created by Fredrik on 20.04.22.
 //
 
-//import SwiftUI
-//
-//struct CoordinateEditor<CoordinateCardinal>: View {
-//    @Binding var degrees: Degrees
-//
-//    var body: some View {
-//        EnumPicker(selection: $)
-//    }
-//}
-//
-//struct CoordinateEditor_Previews: PreviewProvider {
-//    @State static var degrees = .zero
-//    static var previews: some View {
-//        CoordinateEditor<LatitudeCardinal>(degrees: $degrees)
-//    }
-//}
+import SwiftUI
+
+struct CoordinateEditor<CoordinateCardinal>: View
+where CoordinateCardinal: SignedCardinal & Hashable & CaseIterable & CustomStringConvertible,
+      CoordinateCardinal.AllCases: RandomAccessCollection,
+      CoordinateCardinal.AllCases.Index == Int {
+    @Binding var degrees: Degrees
+
+    var body: some View {
+        EnumPicker(selection: Binding(
+            get: { CoordinateCardinal(sign: degrees.sign) },
+            set: { degrees.sign = $0.sign }
+        ))
+    }
+}
+
+struct CoordinateEditor_Previews: PreviewProvider {
+    @State static var degrees = Degrees.zero
+    static var previews: some View {
+        CoordinateEditor<LatitudeCardinal>(degrees: $degrees)
+    }
+}
