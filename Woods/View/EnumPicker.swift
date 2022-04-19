96010fed05f42ea462e28f5ac1bc8daf1bb89704
fwcd <fwcdmail@gmail.com>
2022-04-20 00:40:03 +0200

    Add default initializer to EnumPicker

--- Woods/View/EnumPicker.swift
+++ Woods/View/EnumPicker.swift
@@ -8,25 +8,31 @@
 import SwiftUI
 
 /// A segmented picker that displays all cases from a string-based enum.
-public struct EnumPicker<L, T>: View
+public struct EnumPicker<Label, Value>: View
     where
-        L: View,
-        T: CaseIterable & CustomStringConvertible & Hashable,
-        T.AllCases: RandomAccessCollection,
-        T.AllCases.Index == Int {
-    @Binding private var selection: T
-    private let label: L
+        Label: View,
+        Value: CaseIterable & CustomStringConvertible & Hashable,
+        Value.AllCases: RandomAccessCollection,
+        Value.AllCases.Index == Int {
+    @Binding private var selection: Value
+    private let label: Label
     
-    public init(selection: Binding<T>, label: L) {
+    public init(selection: Binding<Value>, label: Label) {
         self._selection = selection
         self.label = label
     }
     
     public var body: some View {
         Picker(selection: $selection, label: label) {
-            ForEach(0..<T.allCases.count, id: \.self) {
-                Text(T.allCases[$0].description).tag(T.allCases[$0])
+            ForEach(0..<Value.allCases.count, id: \.self) {
+                Text(Value.allCases[$0].description).tag(Value.allCases[$0])
             }
         }
+    }
+}
+
+extension EnumPicker where Label == EmptyView {
+    public init(selection: Binding<Value>) {
+        self.init(selection: selection, label: EmptyView())
     }
 }
