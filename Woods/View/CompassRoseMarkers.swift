72c892959de1ed38df1241959c2a64fdd84870b3
fwcd <fwcdmail@gmail.com>
2022-05-23 05:09:52 +0200

    Factor out CompassRoseMarkers shape

---
+++ Woods/View/CompassRoseMarkers.swift
@@ -0,0 +1,49 @@
+//
+//  CompassRoseMarkers.swift
+//  Woods
+//
+//  Created by Fredrik on 23.05.22.
+//
+
+import SwiftUI
+
+struct CompassRoseMarkers: Shape {
+    var markCount: Int = 180
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let size = rect.size
+        let stride = 2 * .pi / CGFloat(markCount)
+        let markLength = (size.width + size.height) / 20
+        
+        for i in 0..<markCount {
+            let angle = CGFloat(i) * stride
+            let outerRadiusX = size.width / 2
+            let outerRadiusY = size.height / 2
+            let innerRadiusX = outerRadiusX - markLength
+            let innerRadiusY = outerRadiusY - markLength
+            let baseX = cos(angle)
+            let baseY = sin(angle)
+            
+            path.move(to: CGPoint(
+                x: rect.midX + baseX * outerRadiusX,
+                y: rect.midY + baseY * outerRadiusY
+            ))
+            path.addLine(to: CGPoint(
+                x: rect.midX + baseX * innerRadiusX,
+                y: rect.midY + baseY * innerRadiusY
+            ))
+            path.closeSubpath()
+        }
+        
+        return path
+    }
+}
+
+struct CompassRoseMarkers_Previews: PreviewProvider {
+    static var previews: some View {
+        CompassRoseMarkers()
+            .stroke(lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+    }
+}
--- Woods/View/CompassRoseView.swift
+++ Woods/View/CompassRoseView.swift
@@ -8,34 +8,15 @@
 import SwiftUI
 
 struct CompassRoseView: View {
-    var radius: CGFloat = 200
-    var markCount: Int = 180
-    var markLength: CGFloat? = nil
-    var markThickness: CGFloat = 1
-    
     var body: some View {
-        Path { path in
-            let stride = 2 * .pi / CGFloat(markCount)
-            let markLength = markLength ?? (radius / 10)
-            
-            for i in 0..<markCount {
-                let angle = CGFloat(i) * stride
-                let outerRadius = radius
-                let innerRadius = radius - markLength
-                let baseX = cos(angle)
-                let baseY = sin(angle)
-                
-                path.move(to: CGPoint(x: baseX * outerRadius, y: baseY * outerRadius))
-                path.addLine(to: CGPoint(x: baseX * innerRadius, y: baseY * innerRadius))
-                path.closeSubpath()
-            }
-        }
-        .stroke(lineWidth: markThickness)
+        CompassRoseMarkers()
+            .stroke(lineWidth: 2)
     }
 }
 
 struct CompassRoseView_Previews: PreviewProvider {
     static var previews: some View {
         CompassRoseView()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
