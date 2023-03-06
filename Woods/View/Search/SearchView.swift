418a8555d32bb9c9b7da23bfb6d60225e666fa5b
fwcd <fwcdmail@gmail.com>
2023-03-06 01:14:49 +0100

    Add basic search tab implementation

--- Woods/View/Search/SearchView.swift
+++ Woods/View/Search/SearchView.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
+import OSLog
+
+private let log = Logger(subsystem: "Woods", category: "SearchView")
 
 struct SearchView: View {
+    @State private var waypoint: Waypoint? = nil
+    @State private var searchQuery: String = ""
+    
+    @EnvironmentObject private var accounts: Accounts
+    
     var body: some View {
-        Text("TODO")
+        // TODO: Integrate proper search (not just id lookup)
+        NavigationStack {
+            ScrollView {
+                if let waypoint {
+                    WaypointDetailView(waypoint: waypoint)
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchQuery, prompt: "Enter waypoint ID...")
+            .onSubmit(of: .search) {
+                Task {
+                    // TODO: Use all accounts for search results?
+                    if let connector = accounts.accountLogins.values.compactMap(\.connector).first {
+                        do {
+                            waypoint = try await connector.waypoint(id: searchQuery)
+                        } catch {
+                            log.warning("Could not fetch waypoint: \(error)")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
